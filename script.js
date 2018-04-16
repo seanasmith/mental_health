10f607ff7297a64df6ed21a1853c13a955d11217
Glitch (invincible-crocodile) <none>
2018-04-16 07:22:11 +0000

    💕💏 Checkpoint ./index.html:710577/2 ./script.js:710577/802

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
- $('.portrait').on('click', function() {
+/*
+=========================================
+       open student feature popup
+=========================================
+*/
+
+
+//set event and function
+
+$('.portrait').on('click', function() {
     var text = $(this).attr('id');
+  
+//adjust classes when user clicks portrait without closing popup  
     $('.student-text').removeClass('visible');
-    
+    $('.student-text').addClass('hidden');
+  
+//get id for clicked portrait, make visible appropriate text div
+  
     if (text == 'first-story') {
       $('.student-text#story-one-text').removeClass('hidden');
       $('.student-text#story-one-text').addClass('visible');
@@ -15,4 +29,16 @@
     
   });
 
-$(.close)
+/*
+=========================================
+      close student feature popup
+=========================================
+*/
+
+//set event to close icon and remove class visible, add class hidden
+
+$('.close').on('click', function () {
+  $('.student-text').removeClass('visible');
+  $('.student-text').addClass('hidden');
+
+});
