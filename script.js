f3311435cd58a9358fb0e21436a15e33b85fdfbd
Glitch (invincible-crocodile) <none>
2018-04-16 07:01:56 +0000

    🏹⛽️ Checkpoint ./styles.css:710577/297 ./script.js:710577/519

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('hi');
+ $('.portrait').on('click', function() {
+    console.log( $(this).attr('data-food') );
+  });
+  
+  We can then use that element's attributes to do other things:
+  
+   $('.list-group-item').on('mouseover', function() {
+    var food = $(this).attr('data-food');
+    
+    if (food == 'cauliflower') {
+      $('p.first').text('CAULIFLOWER!');
+    } else {
+      $('p.first').text('Four score and seven years ago, our forefathers ...');    
+    }
+    
+  });
